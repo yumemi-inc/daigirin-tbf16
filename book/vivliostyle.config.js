ae13bb7b48631fc6b33fd99bf6d94d1cbb909334
Takumi Muraishi <mrs1669.develop@gmail.com>
2024-04-24 18:56:08 +0900

    fix 記事の順序

--- book/vivliostyle.config.js
+++ book/vivliostyle.config.js
@@ -19,10 +19,10 @@ module.exports = {
     'iseki.md',
     'omori.md',
     'hajimism-feedback-philosophy.md',
-    'muraishi.md',
     'kawashima.md',
     'yusuga.md',
     'kotetu.md',
+    'muraishi.md',
 
     // 著者紹介
     'authors.md',
